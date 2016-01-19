77fdc5e423248b3c53cb94c2b1b20ef6f74038c0
Esmé Cowles <escowles@ticklefish.org>
2016-01-19 09:47:12 -0500

    Adding link from exhibit home page to dashboard - closes #28

---
+++ spec/features/exhibit_home_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'Exhibit Home Page', type: :feature do
+  let(:exhibit) { FactoryGirl.create(:exhibit) }
+
+  context 'a logged in site admin' do
+    let(:user) { FactoryGirl.create(:site_admin) }
+    before(:each) do
+      sign_in user
+    end
+
+    scenario 'site admins see dashboard and edit buttons' do
+      visit spotlight.exhibit_root_path exhibit
+      expect(page).to have_selector 'a.btn', 'Dashboard'
+      expect(page).to have_selector 'a.btn', 'Edit'
+    end
+  end
+
+  context "while not logged in" do
+    scenario "it doesn't show links to create exhibits" do
+      visit spotlight.exhibit_root_path exhibit
+      expect(page).not_to have_link 'Dashboard'
+      expect(page).not_to have_link 'Edit'
+    end
+  end
+end
