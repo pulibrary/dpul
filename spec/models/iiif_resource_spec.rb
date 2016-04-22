dba68577262462fdb407dca02ba6799ff9834ce6
Trey Pendragon <tpendragon@princeton.edu>
2016-04-22 09:56:57 -0700

    Index parent ID

--- spec/models/iiif_resource_spec.rb
+++ spec/models/iiif_resource_spec.rb
@@ -13,5 +13,20 @@ describe IIIFResource do
       expect(solr_doc["full_title_ssim"]).to eq 'Christopher and his kind, 1929-1939'
       expect(solr_doc["date-created_tesim"]).to eq ['1976', '2010']
     end
+    context "when given a MVW", vcr: { cassette_name: 'mvw' } do
+      let(:url) { "https://hydra-dev.princeton.edu/concern/multi_volume_works/f4752g76q/manifest" }
+      it "ingests both items as individual solr records, marking the child" do
+        exhibit = Spotlight::Exhibit.create title: 'Exhibit A'
+        resource = described_class.new manifest_url: url, exhibit: exhibit
+        expect(resource.save_and_index).to be_truthy
+
+        docs = Blacklight.default_index.connection.get("select", params: { q: "*:*" })["response"]["docs"]
+        expect(docs.length).to eq 2
+        scanned_resource_doc = docs.find { |x| x["full_title_ssim"] == ["SR1"] }
+        mvw_doc = docs.find { |x| x["full_title_ssim"] == ["MVW"] }
+        expect(scanned_resource_doc["collection_id_ssim"]).to eq [mvw_doc["id"]]
+        expect(mvw_doc["collection_id_ssim"]).to eq nil
+      end
+    end
   end
 end
