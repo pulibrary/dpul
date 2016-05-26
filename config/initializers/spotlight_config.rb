bfe020433c5e724f1e3243ae4c1fcb5b344b0707
Trey Pendragon <tpendragon@princeton.edu>
2016-05-26 12:11:26 -0700

    Browse categories should display list view.

--- config/initializers/spotlight_config.rb
+++ config/initializers/spotlight_config.rb
@@ -1,3 +1,4 @@
 Spotlight::Engine.config.thumbnail_field = :thumbnail_ssim
 Spotlight::Engine.config.document_presenter_class = RTLPresenter
+Spotlight::Engine.config.default_browse_index_view_type = :list
 Spotlight::Resources::Iiif::Engine.config.metadata_class = -> { ManifestMetadata }
