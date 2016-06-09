f4610bd3e2c803d7efae5dd48f2efa0372e3acf6
Trey Pendragon <tpendragon@princeton.edu>
2016-06-09 14:24:08 -0700

    Group like documents and adjust count

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -11,15 +11,19 @@ class CatalogController < ApplicationController
     config.view.masonry.partials = [:index]
     config.view.slideshow.partials = [:index]
 
-    config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-    # config.show.partials.insert(1, :openseadragon)
+    config.show.tile_source_field = :tile_source_ssim
     config.show.partials.insert(1, :universal_viewer)
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
       qt: 'search',
       rows: 10,
-      fl: '*'
+      fl: '*',
+      group: true,
+      'group.main': true,
+      'group.limit': 1,
+      'group.field': Spotlight::Resources::Iiif::Engine.config.iiif_manifest_field,
+      'group.facet': true
     }
 
     config.document_solr_path = 'get'
@@ -32,10 +36,13 @@ class CatalogController < ApplicationController
     config.add_search_field 'all_fields', label: 'Everything'
 
     config.add_sort_field 'relevance', sort: 'score desc', label: 'Relevance'
+
+    config.add_facet_field 'spotlight_resource_type_ssim'
     config.index.thumbnail_field = 'thumbnail_ssim'
 
     config.add_facet_fields_to_solr_request!
     config.add_field_configuration_to_solr_request!
     config.document_presenter_class = RTLPresenter
+    config.response_model = AdjustedGroupedResponse
   end
 end
