6a65b5f2fa129e3724092b4384dbb378c886d56e
Eliot Jordan <eliotj@princeton.edu>
2016-10-14 13:47:43 -0500

    configure sneakers to use maxretry handler

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,8 +1,20 @@
 require 'sneakers'
+require 'sneakers/handlers/maxretry'
 require_relative 'pom_config'
 Sneakers.configure(
   amqp: Pomegranate.config["events"]["server"],
   exchange: Pomegranate.config["events"]["exchange"],
-  exchange_type: :fanout
+  exchange_type: :fanout,
+  handler: Sneakers::Handlers::Maxretry
 )
 Sneakers.logger.level = Logger::INFO
+
+WORKER_OPTIONS = {
+  ack: true,
+  threads: 10,
+  prefetch: 10,
+  timeout_job_after: 60,
+  heartbeat: 5,
+  amqp_heartbeat: 10,
+  retry_timeout: 60 * 1000 # 60 seconds
+}.freeze
