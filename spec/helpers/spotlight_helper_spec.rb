c97975ddd994ae0bd6ba25146672cc004456b545
Esmé Cowles <escowles@ticklefish.org>
2016-12-22 06:25:44 -0500

    Test coverage

---
+++ spec/helpers/spotlight_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe SpotlightHelper do
+  let(:helper) { TestingHelper.new }
+  before do
+    class TestingHelper
+      include SpotlightHelper
+      include ActionView::Helpers::TagHelper
+    end
+  end
+  after do
+    Object.send(:remove_const, :TestingHelper)
+  end
+
+  describe '#render_document_heading' do
+    let(:presenter) { instance_double('RTLShowPresenter', class: RTLShowPresenter) }
+
+    before do
+      allow(helper).to receive(:presenter).and_return(presenter)
+      allow(presenter).to receive(:header).and_return('title')
+    end
+
+    it 'returns a single ltr span tag' do
+      tag = helper.render_document_heading(title: 'title')
+      expect(tag).to eq '<h4 itemprop="name">title</h4>'
+    end
+  end
+end
