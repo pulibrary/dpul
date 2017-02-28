88512ce3b86e423e0eddee885384e6b0ea2b8835
Trey Pendragon <tpendragon@princeton.edu>
2017-02-28 15:13:23 -0800

    Have everything come off the root.

--- spec/routing/exhibits_routing_spec.rb
+++ spec/routing/exhibits_routing_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe "exhibit routing" do
   describe "POST /exhibits" do
     it "routes to local exhibits controller" do
-      expect(post("/spotlight")).to route_to controller: "exhibits", action: "create"
+      expect(post("/")).to route_to controller: "exhibits", action: "create"
+    end
+  end
+
+  describe "GET /catalog" do
+    it "routes to the catalog controller" do
+      expect(get("/catalog")).to route_to controller: "catalog", action: "index"
     end
   end
 end
