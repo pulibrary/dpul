18fa3b85eae2d749d4b89317ba2ac6806059205c
Trey Pendragon <tpendragon@princeton.edu>
2017-03-10 15:19:11 -0800

    Fix sign out path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   mount Spotlight::Resources::Iiif::Engine, at: 'spotlight_resources_iiif'
   mount Blacklight::Oembed::Engine, at: 'oembed'
 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:passwords, :registration]
+  devise_scope :user do
+    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'users/auth/cas', to: 'users/omniauth_authorize#passthru', defaults: { provider: :cas }, as: "new_user_session"
+  end
+
   mount Blacklight::Engine => '/'
   root to: 'spotlight/exhibits#index'
   resources :exhibits, path: '/', only: [:create, :destroy]
@@ -27,12 +33,6 @@ Rails.application.routes.draw do
     collection do
       delete 'clear'
     end
-  end
-
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: [:passwords, :registration]
-  devise_scope :user do
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-    get 'users/auth/cas', to: 'users/omniauth_authorize#passthru', defaults: { provider: :cas }, as: "new_user_session"
   end
 
   # Dynamic robots.txt
