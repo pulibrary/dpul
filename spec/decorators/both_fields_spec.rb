2fe1de8229ef77f1363fb8c5af374a170da86c41
Trey Pendragon <tpendragon@princeton.edu>
2018-02-23 17:06:54 -0800

    Add missing spec.

---
+++ spec/decorators/both_fields_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe BothFields do
+  subject(:decorator) { described_class.new(field) }
+  let(:field) { instance_double(Spotlight::CustomField, field: field_name) }
+  describe "#alternate_field" do
+    context "when given a tesim field" do
+      let(:field_name) { "test_tesim" }
+      it "returns _ssim" do
+        expect(decorator.alternate_field).to eq "test_ssim"
+      end
+    end
+    context "when given a ssim field" do
+      let(:field_name) { "test_ssim" }
+      it "returns _tesim" do
+        expect(decorator.alternate_field).to eq "test_tesim"
+      end
+    end
+  end
+end
