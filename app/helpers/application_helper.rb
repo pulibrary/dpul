04d2669e38c9945733975da2c2ad0aaa598c874d
Anna Headley <anna3LC@gmail.com>
2018-06-22 13:02:42 -0400

    Restore sidebar to main site conditionally on it having content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
   include Blacklight::BlacklightHelperBehavior
   include Spotlight::ApplicationHelper
+
+  def site_sidebar?
+    can?(:manage, Spotlight::Site.instance) || can?(:create, Spotlight::Exhibit)
+  end
 end
