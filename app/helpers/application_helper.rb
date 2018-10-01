39519a4806b18b7ff9ef9cd4d956ba4e21792a82
Shaun Ellis <shaun@sdellis.com>
2018-10-01 15:10:29 -0400

    removes restrictions for deploy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,4 @@ module ApplicationHelper
     can?(:manage, Spotlight::Site.instance) || can?(:create, Spotlight::Exhibit)
   end
 
-  # site_title pulls from the db if configured through the UI.
-  #   otherwise use the val from the blacklight locale file.
-  #   We need this because application_name helper changes to include exhibit titles
-  def header_title
-    site_title || t("blacklight.application_name")
-  end
-
-  def current_year
-    Date.today.year
-  end
 end
