557ebadf6aaa6fae4bc558286eb764962a847562
Anna Headley <anna3LC@gmail.com>
2018-11-28 16:54:29 -0500

    Update capybara settings to make javascript tests possible
    modeled on figgy's config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,11 +15,19 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
+    chromeOptions: { args: %w[headless disable-gpu disable-setuid-sandbox window-size=7680,4320] }
   )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+
+  http_client = Selenium::WebDriver::Remote::Http::Default.new
+  http_client.read_timeout = 120
+  http_client.open_timeout = 120
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities,
+                                 http_client: http_client)
 end
 Capybara.javascript_driver = :headless_chrome
+Capybara.default_max_wait_time = 15
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
