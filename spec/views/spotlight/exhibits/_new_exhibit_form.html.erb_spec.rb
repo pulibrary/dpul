5c925ccc02c319dfc11bd2ec35bec4b5606b4896
Trey Pendragon <tpendragon@princeton.edu>
2019-02-19 13:55:45 -0800

    Replace VCR in new_exhibit_form

--- spec/views/spotlight/exhibits/_new_exhibit_form.html.erb_spec.rb
+++ spec/views/spotlight/exhibits/_new_exhibit_form.html.erb_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "spotlight/exhibits/_new_exhibit_form.html.erb", vcr: { cassette_name: 'all_collections' } do
+RSpec.describe "spotlight/exhibits/_new_exhibit_form.html.erb" do
+  before do
+    VCR.turn_off!
+    WebMock.disable_net_connect!(allow_localhost: true)
+    stub_collections(fixture: "collections.json")
+  end
+
+  after do
+    VCR.turn_on!
+  end
   it "displays a select of all available exhibits" do
     assign(:exhibit, Spotlight::Exhibit.new)
     render
