451f119b09af101872714f60bccc8653c5143934
Trey Pendragon <tpendragon@princeton.edu>
2019-02-19 13:09:45 -0800

    Replace external_manifest VCR

--- spec/models/external_manifest_spec.rb
+++ spec/models/external_manifest_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe ExternalManifest, vcr: { cassette_name: "all_collections" } do
+RSpec.describe ExternalManifest do
+  before do
+    VCR.turn_off!
+    WebMock.disable_net_connect!(allow_localhost: true)
+    stub_collections(fixture: "collections.json")
+  end
+  after do
+    VCR.turn_on!
+  end
   describe ".load" do
     subject(:manifest) { described_class.load(manifest_url) }
     let(:manifest_url) { "https://hydra-dev.princeton.edu/iiif/collections" }
