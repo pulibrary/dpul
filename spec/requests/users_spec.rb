40c892c3b8eddce704242a642879153e678bc7d2
GriffinJ <jrgriffiniii@gmail.com>
2019-03-21 15:27:12 -0400

    Extending test coverage for User requests

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "User requests", type: :request do
+  let(:token) do
+    OpenStruct.new(
+      provider: "cas",
+      uid: "test"
+    )
+  end
+  let(:params) do
+    {
+      "omniauth.auth" => token
+    }
+  end
+
+  describe "#user_cas_omniauth_callback" do
+    it "redirects to a login and renders an alert" do
+      post "/users/auth/cas/callback", params: params
+      expect(response).to redirect_to(root_url)
+      expect(flash[:notice]).to eq "Successfully authenticated from CAS account."
+    end
+  end
+end
