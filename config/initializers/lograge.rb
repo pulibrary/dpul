24e2a68070824c4e4ebf3f3eedc7c391205e946d
Trey Pendragon <tpendragon@princeton.edu>
2019-04-12 11:32:35 -0700

    Fix lograge crash of UploadedFile

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 Rails.application.configure do
+  class ActionDispatch::Http::UploadedFile
+    def as_json(_options = nil)
+      %w[headers].each_with_object({}) do |attr, hash|
+        hash[attr] = send(attr).force_encoding('utf-8')
+      end
+    end
+  end
   # Lograge config
   config.lograge.enabled = true
 
@@ -9,6 +16,6 @@ Rails.application.configure do
   # This is is useful if you want to log query parameters
   config.lograge.custom_options = lambda do |event|
     { ddsource: ["ruby"],
-      params: event.payload[:params].reject { |k| %w(controller action).include? k } }
+      params: event.payload[:params].reject { |k| %w[controller action format].include? k } }
   end
 end
