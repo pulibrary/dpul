53d712287edbc90861e1d07ec763a4da3e5141de
Anna Headley <anna3LC@gmail.com>
2019-09-25 10:53:17 -0400

    Replace factory in bookmarks feature test, add path context spec

--- spec/factories/solr_documents.rb
+++ spec/factories/solr_documents.rb
@@ -1,3 +1,6 @@
+# SolrDoc is a pretty weird sort of object; you can set values in a hash passed
+# to new but you can't set them later. Advise to use SolrDocument.new directly
+# when you need anything more than id.
 FactoryBot.define do
   factory :solr_document, class: SolrDocument, aliases: [:document] do
     sequence(:id) { |n| n.to_s }
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,32 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'Bookmarks', type: :feature, js: true do
-  let(:id) { "67890" }
-  let(:document) { FactoryBot.build(:document, id: id) }
-
-  before do
-    FactoryBot.create(
-      :sidecar,
-      document: document,
-      with_indexed_document: true
-    )
-    FactoryBot.create(
-      :sidecar,
-      with_indexed_document: true
-    )
-    Blacklight.default_index.connection.commit
+  let(:iiif_resource1) do FactoryBot.create(
+    :iiif_resource,
+    url: "https://figgy.princeton.edu/concern/scanned_resources/beaec815-6a34-4519-8ce8-40a89d3b1956/manifest",
+    exhibit: exhibit,
+    manifest_fixture: "paris_map.json",
+    source_metadata_identifier: "beaec815-6a34-4519-8ce8-40a89d3b1956",
+    spec: self
+  )
   end
 
+  let(:iiif_resource2) do FactoryBot.create(
+    :iiif_resource,
+    url: "https://figgy.princeton.edu/concern/scanned_resources/0cc43bdb-ae21-47b2-90bc-bc21a18ee821/manifest",
+    exhibit: exhibit,
+    manifest_fixture: "chinese_medicine.json",
+    source_metadata_identifier: "0cc43bdb-ae21-47b2-90bc-bc21a18ee821",
+    spec: self
+  )
+  end
+
+  let(:exhibit) { FactoryBot.create(:exhibit) }
+  let(:solr_id) { iiif_resource1.document_builder.documents_to_index.first[:id] }
+  let(:title) { "Plan de Paris : commencé de l'année 1734" }
+
   context "when checking the bookmark box" do
-    it "Adds bookmarks to the bookmark page" do
-      visit "/catalog?search_field=all_fields&q="
-      bookmark_box = "toggle_bookmark_#{id}"
+    it "Adds bookmarks to the bookmark page, shows bookmark in exhibit context" do
+      iiif_resource1
+      iiif_resource2
+      visit spotlight.search_exhibit_catalog_path(exhibit, search_field: 'all_fields', q: '')
+      bookmark_box = "toggle_bookmark_#{solr_id}"
       check bookmark_box
       expect(page).to have_content "In Bookmarks"
 
       visit "/bookmarks"
       expect(page).to have_content "1 entry found"
       expect(page).to have_link "CSV"
+
+      page.click_on title
+      expect(page).to have_current_path spotlight.exhibit_solr_document_path(exhibit, iiif_resource1.noid)
 
       visit "/catalog?search_field=all_fields&q="
       expect(page).to have_content "In Bookmarks"
