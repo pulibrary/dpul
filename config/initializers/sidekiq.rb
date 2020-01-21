49f76c2f20acb2b7120123346fb70260aaf8cdc7
Trey Pendragon <tpendragon@princeton.edu>
2020-01-21 13:11:15 -0800

    Redis configuration. (#694)
    Closes #691

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require_relative "redis_config"
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'pomegranate' }
+  config.redis = Redis.current.client.options
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'pomegranate' }
+  config.redis = Redis.current.client.options
 end
