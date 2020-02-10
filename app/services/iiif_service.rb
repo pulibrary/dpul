451f1ff2749c8ef074e1a6560f1d3a5391c8f5a6
Trey Pendragon <tpendragon@princeton.edu>
2020-02-10 11:53:35 -0800

    Support indexing Recording IIIF 3 manifests.

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -15,6 +15,29 @@ class IiifService < ::Spotlight::Resources::IiifService
   end
 
   def create_iiif_manifest(manifest, collection = nil)
-    IiifManifest.new(url: manifest['@id'], manifest: manifest, collection: collection)
+    IiifManifest.new(url: manifest['@id'] || manifest['id'], manifest: manifest, collection: collection)
+  end
+
+  def object
+    @object ||= ManifestParser.parse(JSON.parse(response))
+  end
+
+  def manifest?
+    object.is_a?(IIIF::Presentation::Manifest) || object.is_a?(ManifestParser::IIIF3Manifest)
+  end
+
+  class ManifestParser
+    def self.parse(json)
+      return IIIF::Service.parse(json.to_json) unless json["@context"]&.include?("http://iiif.io/api/presentation/3/context.json")
+      IIIF3Manifest.new(json)
+    end
+
+    # IIIF::Presentation doesn't support IIIF 3. Fortunately we don't use much
+    # of it, so a simple wrapper with a label for a manifest gets us there.
+    class IIIF3Manifest < SimpleDelegator
+      def label
+        Array.wrap(__getobj__["label"]).first["@none"]
+      end
+    end
   end
 end
