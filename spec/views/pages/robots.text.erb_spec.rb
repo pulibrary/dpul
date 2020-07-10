1aaa2a046e5b8d644297aa7cb5e5413749f5342a
Anna Headley <anna.headley@gmail.com>
2020-07-10 12:29:46 -0400

    Try Disallow: /*?*, as used in orangelight
    fixes #811

--- spec/views/pages/robots.text.erb_spec.rb
+++ spec/views/pages/robots.text.erb_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "pages/robots.text.erb" do
     end
 
     it "slows down spiders and disallows indexing anything with a query-string" do
-      expect(response).to include "Disallow: /*?"
+      expect(response).to include "Disallow: /*?*"
       expect(response).to include "Crawl-delay: 10"
     end
   end
