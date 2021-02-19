365adbc062c2f8a8b984649fba75300db8e4c14f
Anna Headley <anna.headley@gmail.com>
2021-02-19 16:04:02 -0500

    Test adding a theme

--- config/initializers/spotlight_config.rb
+++ config/initializers/spotlight_config.rb
@@ -5,6 +5,7 @@ Spotlight::Engine.config.default_browse_index_view_type = :list
 Spotlight::Engine.config.upload_fields = []
 Spotlight::Engine.config.iiif_metadata_class = -> { ManifestMetadata }
 Spotlight::Engine.config.iiif_manifest_field = :content_metadata_iiif_manifest_field_ssi
+Spotlight::Engine.config.exhibit_themes = %w[driver default]
 
 Spotlight::Engine.config.sir_trevor_widgets = %w[
   Heading Text List Quote Iframe Video Oembed Rule UploadedItems Browse
