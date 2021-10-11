312149afbfb1120bf67a6658ea9c5c5f48036c7d
Trey Pendragon <tpendragon@princeton.edu>
2021-10-11 16:33:33 -0700

    Add comment.

--- app/processors/list_rendering.rb
+++ app/processors/list_rendering.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-# Joins values using configured value or linebreak
+# In show views we need to render fields as a list so that we can align RTL text
+# to the right. This special piece of the rendering pipeline builds that list
+# for us.
+#
+# This is registered in config/initializers/blacklight_initializer.rb.
 class ListRendering < Blacklight::Rendering::AbstractStep
   include ActionView::Helpers::TextHelper
 
   def render
     # If it's a text area, don't make it a list.
     if config.text_area == "1" || context.try(:action_name) != "show"
-      # Index page should use join separator.
+      # Index page should use join step.
       join_result = Blacklight::Rendering::Join.new(values, config, document, context, options, [Blacklight::Rendering::Terminator]).render
       next_step(join_result)
     else
