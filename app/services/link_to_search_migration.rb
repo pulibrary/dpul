0a8bf973447148c0fb4edbdc4f941bfa0ca53f0f
Anna Headley <anna.headley@gmail.com>
2021-10-12 14:56:19 -0400

    Migrate link_to_search to link_to_facet
    We had it flipped
    
    closes #965
    
    Co-authored-by: Eliot Jordan <eliot.jordan@gmail.com>

--- app/services/link_to_search_migration.rb
+++ app/services/link_to_search_migration.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class LinkToSearchMigration
-  # Migrates `link_to_facet` field in blacklight configurations to
-  # `link_to_search` for compatibility with Blacklight 7.
+  # Migrates `link_to_search` field in blacklight configurations to
+  # `link_to_facet` for compatibility with Blacklight 7.
   def self.run
     Spotlight::BlacklightConfiguration.all.each do |config|
-      fields = config.index_fields.select { |_k, v| v["link_to_facet"] }
+      fields = config.index_fields.select { |_k, v| v["link_to_search"] }
       fields.keys.each do |key|
-        config.index_fields[key]["link_to_search"] = config.index_fields[key].delete("link_to_facet")
+        config.index_fields[key]["link_to_facet"] = config.index_fields[key].delete("link_to_search")
       end
       config.save
     end
--- spec/services/link_to_search_migration_spec.rb
+++ spec/services/link_to_search_migration_spec.rb
@@ -6,16 +6,16 @@ describe LinkToSearchMigration do
   before do
     FactoryBot.create(:exhibit)
     Spotlight::BlacklightConfiguration.all.each do |config|
-      config.index_fields["first_field_ssm"] = { "link_to_facet" => "first" }
+      config.index_fields["first_field_ssm"] = { "link_to_search" => "first" }
       config.save
     end
   end
 
-  it 'replaces the link_to_facet field name with link_to_search' do
+  it 'replaces the link_to_search field name with link_to_facet' do
     described_class.run
 
     config = Spotlight::BlacklightConfiguration.all.first
     first_field = config.index_fields["first_field_ssm"]
-    expect(first_field["link_to_search"]).to eq "first"
+    expect(first_field["link_to_facet"]).to eq "first"
   end
 end
