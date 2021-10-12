c0cc91ac6aa78874cecc6bdfd528aad474cbeeff
Eliot Jordan <eliotj@princeton.edu>
2021-10-12 13:13:29 -0500

    Moved find_or_initialize_job_tracker to a monkey patch
    Co-authored-by: Anna Headley <anna.headley@gmail.com>

---
+++ config/initializers/monkeypatches/job_tracking_patch.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module JobTrackingPatch
+  # Overrode this to do find_or_initialize_by - this may be broken upstream.
+  # See: https://github.com/projectblacklight/spotlight/issues/2777
+  # Needed for Spotlight::ReindexJob and Spotlight::ReindexExhibitJob
+  def find_or_initialize_job_tracker
+    Spotlight::JobTracker.find_or_initialize_by(job_id: job_id) do |tracker|
+      tracker.job_class = self.class.name
+      tracker.status = 'enqueued'
+    end
+  end
+end
+
+Spotlight::JobTracking.prepend(JobTrackingPatch)
