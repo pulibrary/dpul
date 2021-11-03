a15a757464c8d2f370aca0f773d22ad7630dec65
Shaun Ellis <shaun@sdellis.com>
2021-11-03 16:50:04 -0400

    changes limit your search icon from hamburger to filter

---
+++ app/components/blacklight/response/facet_group_component.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+# this replicates upstream file in order to override the ViewComponent erb
+
+module Blacklight
+  module Response
+    # Render a group of facet fields
+    class FacetGroupComponent < ::ViewComponent::Base
+      # @param [Blacklight::Response] response
+      # @param [Array<String>] fields facet fields to render
+      # @param [String] title the title of the facet group section
+      # @param [String] id a unique identifier for the group
+      def initialize(response:, fields: [], title: nil, id: nil)
+        @response = response
+        @fields = fields
+        @title = title
+        @id = id ? "facets-#{id}" : 'facets'
+        @panel_id = id ? "facet-panel-#{id}-collapse" : 'facet-panel-collapse'
+      end
+
+      def render?
+        Deprecation.silence(Blacklight::FacetsHelperBehavior) do
+          @view_context.has_facet_values?(@fields, @response)
+        end
+      end
+    end
+  end
+end
