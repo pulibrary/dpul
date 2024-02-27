b5ba6534290b93591b2bf358cc416c3c03efd572
Trey Pendragon <tpendragon@princeton.edu>
2024-02-26 17:08:38 -0800

    Fix more/less handling for lists.
    While I was at it I made it default to "more" and made the toggle work a
    little better. There's probably more we could do here, but...eh, YAGNI.
    
    Closes #1499

--- app/javascript/dpul/more_link.js
+++ app/javascript/dpul/more_link.js
@@ -2,19 +2,21 @@ export default class MoreLink {
   constructor() {
     let value_id = $("#document").data("document-id");
     let limitCharacters = 1200;
-    let dl_children = $("#doc_"+value_id).find('dd');
+    // Get all children that only have text. Sometimes these are 'dd', sometimes
+    // these are 'li'
+    let dl_children = $("#doc_"+value_id).find('*:not(:has(*))');
     dl_children.each(function(i, item){
-        let char_length = $(this).text().length;
-        if (char_length > limitCharacters) {
-           let displayedCharacters = $(this).text().substr(0,limitCharacters);
-           let hiddenCharacters = $(this).text().substr(limitCharacters, char_length - limitCharacters );
-           let html_ul_li =
-           displayedCharacters +
-           '<span class="morecontent">' + hiddenCharacters +
-           '</span>&nbsp;&nbsp;<span><a href="" class="morelink">' + " ... Less" + '</a></span>';
-           $(this).html(html_ul_li);
-           $(".morecontent span").hide();
-        }
+      let char_length = $(this).text().length;
+      if (char_length > limitCharacters) {
+        let displayedCharacters = $(this).text().substr(0,limitCharacters);
+        let hiddenCharacters = $(this).text().substr(limitCharacters, char_length - limitCharacters );
+        let html_ul_li =
+          displayedCharacters +
+          '<span class="morecontent">' + hiddenCharacters +
+          '</span>&nbsp;&nbsp;<span><a href="" class="morelink less">' + " ... More" + '</a></span>';
+        $(this).html(html_ul_li);
+        $("span.morecontent").hide();
+      }
     })
 
      $(".morelink").click(function(){
