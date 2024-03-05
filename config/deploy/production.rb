4d1a0ec364a97e0952fb745fe249b5f57798e647
Trey Pendragon <tpendragon@princeton.edu>
2024-03-05 10:27:46 -0800

    Upgrade DPUL to Jammy.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,8 +8,8 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'dpul1.princeton.edu', user: 'deploy', roles: %w(app db web worker)
-server 'dpul2.princeton.edu', user: 'deploy', roles: %w(app web worker)
+server 'dpul-prod1.princeton.edu', user: 'deploy', roles: %w(app db web worker)
+server 'dpul-prod2.princeton.edu', user: 'deploy', roles: %w(app web worker)
 set :deploy_to, '/opt/dpul'
 
 # role-based syntax
