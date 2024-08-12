f776b529f1203034acc3b9a24fc1eab3fd353bb6
Trey Pendragon <tpendragon@princeton.edu>
2024-08-12 12:53:33 -0700

    Get rails c running.

--- config/initializers/monkeypatches/blacklight_configuration_patch.rb
+++ config/initializers/monkeypatches/blacklight_configuration_patch.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-# Add the full image url to the autocomplete response
-module BlacklightConfigurationPatch
-  def default_autocomplete_field_list(config)
-    [
-      super,
-      Spotlight::Engine.config.full_image_field
-    ].flatten.join(' ')
+Rails.application.config.to_prepare do
+  # Add the full image url to the autocomplete response
+  module BlacklightConfigurationPatch
+    def default_autocomplete_field_list(config)
+      [
+        super,
+        Spotlight::Engine.config.full_image_field
+      ].flatten.join(' ')
+    end
   end
-end
 
-Spotlight::BlacklightConfiguration.prepend(BlacklightConfigurationPatch)
+  Spotlight::BlacklightConfiguration.prepend(BlacklightConfigurationPatch)
+end
