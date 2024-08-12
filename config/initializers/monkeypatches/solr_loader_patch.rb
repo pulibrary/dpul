91fc13f89d59080e0ebd069f6d567bb5726b3de3
Trey Pendragon <tpendragon@princeton.edu>
2024-08-12 12:53:33 -0700

    Get rails c running.

--- config/initializers/monkeypatches/solr_loader_patch.rb
+++ config/initializers/monkeypatches/solr_loader_patch.rb
@@ -3,14 +3,16 @@
 # Patches send_one to only send two arguments to Etl::Executor#on_error
 # TODO: Remove when fixed upstream. See
 #   https://github.com/projectblacklight/spotlight/issues/2778
-module SolrLoaderPatch
-  def send_one(document, pipeline)
-    blacklight_solr.update params: { commitWithin: 500 },
-                           data: [document].to_json,
-                           headers: { 'Content-Type' => 'application/json' }
-  rescue StandardError => e
-    pipeline&.on_error(e, document.to_json)
+Rails.application.config.to_prepare do
+  module SolrLoaderPatch
+    def send_one(document, pipeline)
+      blacklight_solr.update params: { commitWithin: 500 },
+        data: [document].to_json,
+        headers: { 'Content-Type' => 'application/json' }
+    rescue StandardError => e
+      pipeline&.on_error(e, document.to_json)
+    end
   end
-end
 
-Spotlight::Etl::SolrLoader.prepend(SolrLoaderPatch)
+  Spotlight::Etl::SolrLoader.prepend(SolrLoaderPatch)
+end
