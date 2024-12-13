ac7d5860c3146c4bbe44fbccea2f083371a46aaf
Anna Headley <845363+hackartisan@users.noreply.github.com>
2024-12-13 15:07:19 -0500

    Simplify capybara selenium, add run in browser option
    copied from pulfalight setup
    to see the browser use js: true

--- spec/support/capybara_selenium.rb
+++ spec/support/capybara_selenium.rb
@@ -3,18 +3,11 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
-# there's a bug in capybara-screenshot that requires us to name
-#   the driver ":selenium" so we changed it from :headless_chrome"
-Capybara.register_driver :selenium do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(
-    args: %w[headless disable-gpu no-sandbox window-size=1536,1152]
-  )
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    clear_session_storage: true,
-    clear_local_storage: true,
-    options:
-end
-
-Capybara.javascript_driver = :selenium
+Capybara.default_driver = :rack_test
+Capybara.javascript_driver =
+  if ENV["RUN_IN_BROWSER"] == "true"
+    :selenium_chrome
+  else
+    :selenium_chrome_headless
+  end
 Capybara.default_max_wait_time = 15
