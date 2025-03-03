a02796ea3c74be3f979ce08130f82df506bf3e2c
Anna Headley <845363+hackartisan@users.noreply.github.com>
2025-03-03 10:08:41 -0500

    Align metadata field view with upstream 4.0

--- spec/views/spotlight/metadata_configurations/_metadata_field.html.erb_spec.rb
+++ spec/views/spotlight/metadata_configurations/_metadata_field.html.erb_spec.rb
@@ -17,12 +17,18 @@ RSpec.describe 'spotlight/metadata_configurations/_metadata_field', type: :view 
   end
 
   let(:facet_field) { exhibit.blacklight_configuration.blacklight_config.index_fields["two"] }
-  let(:builder) { ActionView::Helpers::FormBuilder.new 'z', nil, view, {} }
+  let(:f) do
+    form_helper = nil
+    controller.view_context.bootstrap_form_for('z', url: '/update') do |f|
+      form_helper = f
+    end
+    form_helper
+  end
 
   it 'renders a tooltip for imported fields' do
     Spotlight::CustomField.create!(exhibit:, slug: "two", field: "two", label: "two", field_type: "vocab", readonly_field: true)
     allow(view).to receive(:index_field_label).with(nil, 'two').and_return 'Some label'
-    render partial: p, locals: { key: 'two', config: facet_field, f: builder }
+    render partial: p, locals: { key: 'two', config: facet_field, f: f }
 
     expect(rendered).to have_selector '.import-tooltip'
   end
@@ -30,7 +36,7 @@ RSpec.describe 'spotlight/metadata_configurations/_metadata_field', type: :view 
   it "doesn't render a tooltip for writeable fields" do
     Spotlight::CustomField.create!(exhibit:, slug: "two", field: "two", label: "two", field_type: "vocab", readonly_field: false)
     allow(view).to receive(:index_field_label).with(nil, 'two').and_return 'Some label'
-    render partial: p, locals: { key: 'two', config: facet_field, f: builder }
+    render partial: p, locals: { key: 'two', config: facet_field, f: f }
 
     expect(rendered).not_to have_selector '.import-tooltip'
   end
@@ -38,7 +44,7 @@ RSpec.describe 'spotlight/metadata_configurations/_metadata_field', type: :view 
   it "renders a field to set link_to_facet" do
     Spotlight::CustomField.create!(exhibit:, slug: "two", field: "two", label: "two", field_type: "vocab", readonly_field: false)
     allow(view).to receive(:index_field_label).with(nil, 'two').and_return 'Some label'
-    render partial: p, locals: { key: 'two', config: facet_field, f: builder }
+    render partial: p, locals: { key: 'two', config: facet_field, f: f }
 
     expect(rendered).to have_selector "input[type=checkbox][name='z[two][link_to_facet]'][value='two']"
   end
